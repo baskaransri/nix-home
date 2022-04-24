2b1483671f7982beff4f18608774575bea203d11
Baskaran Sripathmanathan <baskaran@Scotts-MacBook-Pro.local>
2022-04-24 16:27:20 +0100

    add flake, doesnt work

--- flake.nix
+++ flake.nix
@@ -1,36 +1,217 @@
 {
-  description = "Home Manager NixOS configuration";
+  #home-manager build --flake './#baskaran'
+  description = "Baskaran's Home manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-21.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-21.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, ... }: {
-    homeConfigurations = {
-      baskaran = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "x86_64-darwin";
-        homeDirectory = "/home/baskaran";
-        username = "baskaran";
-        stateVersion = "21.05";
-        configuration = { config, pkgs, ... }:
-          let
-            overlay-unstable = final: prev: {
-              unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-darwin;
-            };
-          in {
-            nixpkgs.overlays = [ overlay-unstable ];
-            nixpkgs.config = {
-              allowUnfree = true;
-              allowBroken = true;
-            };
+    nixpkgs.url = "flake:nixpkgs";
+    homeManager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
 
-            imports = [ ./users/baskaran/.config/nixpkgs/home.nix ];
+  };
+
+  outputs = { self, nixpkgs, homeManager, emacs-overlay }: {
+    homeConfigurations = {
+      "baskaran" = homeManager.lib.homeManagerConfiguration {
+        configuration = { lib, config, pkgs, ... }: {
+          programs.home-manager.enable = true;
+          home.sessionVariables.EDITOR = "vim";
+          home.packages = with pkgs; [
+            ##General config
+            #Terminal Manager
+            kitty
+            tmux
+
+            #Print to Remarkable stuff
+            qpdf
+            ghostscript
+            rmapi
+
+            ####Dev stuff:
+            niv
+            direnv
+            lorri
+
+            nixfmt
+            nix-prefetch-git
+
+            ###DOOM packages
+            ##Org packages
+            sqlite # for org-roam
+            graphviz
+            #pngpaste
+
+            ##Python packages
+            black # for doom python autoformat
+            nodePackages.pyright # for doom python-lsp
+            ###Python Emacs Debugger packages
+            nodejs
+            python39Packages.debugpy
+
+            ##Julia packages
+
+            ##General Doom Packages
+            ripgrep
+            fd
+            coreutils
+            git
+            clang
+
+            shellcheck
+            pandoc
+
+            #emacs fonts
+            fontconfig
+            emacs-all-the-icons-fonts
+            #(nerdfonts.override { fonts = [ "Inconsolata" "FiraCode" ]; })
+
+            #oh-my-zsh
+            zsh-autosuggestions
+          ];
+          fonts.fontconfig.enable = true;
+          programs.emacs = {
+            enable = true;
+            package = pkgs.emacsNativeComp;
+            extraPackages = (epkgs: [ epkgs.vterm ]);
           };
+
+          #direnv stuff:
+          programs.direnv = {
+            enable = true;
+            nix-direnv.enable = true;
+          };
+
+          programs.zsh = {
+            enable = true;
+            enableSyntaxHighlighting = true;
+            oh-my-zsh = {
+              enable = true;
+              plugins = [
+                "git"
+                #"zsh-autosuggestions"
+                #"zsh-history-substring-search"
+                #"zsh-syntax-highlighting"
+              ];
+              #theme = "robbyrussell";
+            };
+          };
+          programs.starship = {
+            enable = true;
+            enableZshIntegration = true;
+            # Configuration written to ~/.config/starship.toml
+            settings = {
+              add_newline = false;
+              line_break.disabled = true;
+
+              # character = {
+              #   success_symbol = "[➜](bold green)";
+              #   error_symbol = "[➜](bold red)";
+              # };
+
+              #package.disabled = true;
+              custom.haskell = {
+                symbol = "λ";
+                extensions = [ "hs" ];
+                style = "bold purple";
+                format = "\\[[$symbol]($style)\\]";
+              };
+              aws.format =
+                "\\[[$symbol($profile)(\\($region\\))(\\[$duration\\])]($style)\\]";
+              cmake.format = "\\[[$symbol($version)]($style)\\]";
+              cmd_duration.format = "\\[[⏱ $duration]($style)\\]";
+              cobol.format = "\\[[$symbol($version)]($style)\\]";
+              conda.format = "\\[[$symbol$environment]($style)\\]";
+              crystal.format = "\\[[$symbol($version)]($style)\\]";
+              dart.format = "\\[[$symbol($version)]($style)\\]";
+              deno.format = "\\[[$symbol($version)]($style)\\]";
+              docker_context.format = "\\[[$symbol$context]($style)\\]";
+              dotnet.format = "\\[[$symbol($version)(🎯 $tfm)]($style)\\]";
+              elixir.format =
+                "\\[[$symbol($version \\(OTP $otp_version\\))]($style)\\]";
+              elm.format = "\\[[$symbol($version)]($style)\\]";
+              erlang.format = "\\[[$symbol($version)]($style)\\]";
+              gcloud.format =
+                "\\[[$symbol$account(@$domain)(\\($region\\))]($style)\\]";
+              git_branch.format = "\\[[$symbol$branch]($style)\\]";
+              git_status.format = "([\\[$all_status$ahead_behind\\]]($style))";
+              golang.format = "\\[[$symbol($version)]($style)\\]";
+              helm.format = "\\[[$symbol($version)]($style)\\]";
+              hg_branch.format = "\\[[$symbol$branch]($style)\\]";
+              java.format = "\\[[$symbol($version)]($style)\\]";
+              julia.format = "\\[[$symbol($version)]($style)\\]";
+              kotlin.format = "\\[[$symbol($version)]($style)\\]";
+              kubernetes.format =
+                "\\[[$symbol$context( \\($namespace\\))]($style)\\]";
+              lua.format = "\\[[$symbol($version)]($style)\\]";
+              memory_usage.format = "\\[$symbol[$ram( | $swap)]($style)\\]";
+              nim.format = "\\[[$symbol($version)]($style)\\]";
+              #nix_shell.format = "\\[[$symbol$state( \\($name\\))]($style)\\]";
+              nix_shell.format = "\\[[$symbol\\($name\\)]($style)\\]";
+              nodejs.format = "\\[[$symbol($version)]($style)\\]";
+              ocaml.format =
+                "\\[[$symbol($version)(\\($switch_indicator$switch_name\\))]($style)\\]";
+              openstack.format =
+                "\\[[$symbol$cloud(\\($project\\))]($style)\\]";
+              package.format = "\\[[$symbol$version]($style)\\]";
+              perl.format = "\\[[$symbol($version)]($style)\\]";
+              php.format = "\\[[$symbol($version)]($style)\\]";
+              pulumi.format = "\\[[$symbol$stack]($style)\\]";
+              purescript.format = "\\[[$symbol($version)]($style)\\]";
+              python.format =
+                "\\[[\${symbol}\${pyenv_prefix}(\${version})(\\($virtualenv\\))]($style)\\]";
+              red.format = "\\[[$symbol($version)]($style)\\]";
+              ruby.format = "\\[[$symbol($version)]($style)\\]";
+              rust.format = "\\[[$symbol($version)]($style)\\]";
+              scala.format = "\\[[$symbol($version)]($style)\\]";
+              sudo.format = "\\[[as $symbol]\\]";
+              swift.format = "\\[[$symbol($version)]($style)\\]";
+              terraform.format = "\\[[$symbol$workspace]($style)\\]";
+              time.format = "\\[[$time]($style)\\]";
+              username.format = "\\[[$user]($style)\\]";
+              vagrant.format = "\\[[$symbol($version)]($style)\\]";
+              vlang.format = "\\[[$symbol($version)]($style)\\]";
+              zig.format = "\\[[$symbol($version)]($style)\\]";
+
+              # This is the "bracketed segments" preset.
+            };
+          };
+
+          # add installs to spotlight search
+          # copied from https://github.com/nix-community/home-manager/issues/1341
+          home.activation = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+            copyApplications = let
+              apps = pkgs.buildEnv {
+                name = "home-manager-applications";
+                paths = config.home.packages;
+                pathsToLink = "/Applications";
+              };
+            in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+              baseDir="$HOME/Applications/Home Manager Apps"
+              if [ -d "$baseDir" ]; then
+                rm -rf "$baseDir"
+              fi
+              mkdir -p "$baseDir"
+              for appFile in ${apps}/Applications/*; do
+                target="$baseDir/$(basename "$appFile")"
+                $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+                $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+              done
+            '';
+          };
+          home.stateVersion = "21.11";
+        };
+
+        pkgs = import nixpkgs {
+          system = "x86_64-darwin";
+          overlays = [ emacs-overlay.overlay ];
+        };
+        system = "x86_64-darwin";
+        homeDirectory = "/home/baskaran/";
+        username = "baskaran";
+        stateVersion = "21.11";
       };
     };
-    baskaran = self.homeConfigurations.baskaran.activationPackage;
-    defaultPackage.x86_64-darwin = self.baskaran;
   };
 }
